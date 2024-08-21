ef3309b2456e5d4adb0e8d6a8465aba12779c0d3
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2024-08-21 18:15:43 +0100

    calender components removed

--- src/pages/dashboard/talent/TalentDashBoard.tsx
+++ src/pages/dashboard/talent/TalentDashBoard.tsx
@@ -9,63 +9,48 @@ const TalentDashBoard: React.FC = () => {
   return (
     <section className="mt-24 px-4 xl:px-10 lg:px-10">
       <div className="mx-auto space-y-10">
-        <div className="flex items-stretch flex-wrap gap-2 flex-col 2xl:flex-row xl:items-start max-w-xl mx-auto md:max-w-full">
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 flex-1 2xl:self-center w-full">
-            <Card className="sm:p-5 shadow flex items-center col-span-full sm:col-span-1 bg-white w-full rounded-xl">
-              <div className="space-y-3 md:space-y-6 w-full">
-                <h3 className=" capitalize font-semibold text-gray-700">
-                  jobs done
-                </h3>
-                <h2 className="block text-base sm:text-xl md:text-3xl lg:text-4xl font-bold text-gray-700">
-                  10
-                </h2>
-                <p className="text-base font-normal text-gray-500">
-                  +30 This week
-                </p>
-              </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 flex-1 2xl:self-center w-full">
+          <Card className="sm:p-5 shadow flex items-center col-span-full sm:col-span-1 bg-white w-full rounded-xl">
+            <div className="space-y-3 md:space-y-6 w-full">
+              <h3 className=" capitalize text-base sm:text-xl font-semibold text-gray-700">jobs done</h3>
+              <h2 className="block text-base sm:text-xl md:text-3xl lg:text-4xl font-bold text-gray-700">
+                10
+              </h2>
+              <p className="text-base font-normal text-gray-500">+30 This week</p>
+            </div>
 
-              <div className="self-end">
-                <GraphIcon className="h-24" />
-              </div>
-            </Card>
+            <div className="self-end">
+              <GraphIcon className="h-32" />
+            </div>
+          </Card>
 
-            <Card className="sm:p-5 shadow flex items-center col-span-full sm:col-span-1 bg-white w-full rounded-xl">
-              <div className="space-y-3 md:space-y-6 w-full">
-                <h3 className="capitalize font-semibold text-gray-700">
-                  pending job request
-                </h3>
-                <h2 className="block text-base sm:text-xl md:text-3xl lg:text-4xl font-bold text-gray-700">
-                  10
-                </h2>
-                <p className="text-base font-normal text-gray-500">
-                  +30 This week
-                </p>
-              </div>
+          <Card className="sm:p-5 shadow flex items-center col-span-full sm:col-span-1 bg-white w-full rounded-xl">
+            <div className="space-y-3 md:space-y-6 w-full">
+              <h3 className="capitalize text-base sm:text-xl font-semibold text-gray-700">applied job</h3>
+              <h2 className="block text-base sm:text-xl md:text-3xl lg:text-4xl font-bold text-gray-700">
+                10
+              </h2>
+              <p className="text-base font-normal text-gray-500">+30 This week</p>
+            </div>
 
-              <div className="self-end">
-                <GraphIcon className="h-24" />
-              </div>
-            </Card>
+            <div className="self-end">
+              <GraphIcon className="h-32" />
+            </div>
+          </Card>
 
-            <Card className="sm:p-5 shadow flex items-center col-span-full sm:col-span-1 bg-white w-full rounded-xl">
-              <div className="space-y-3 md:space-y-6 w-full">
-                <h3 className=" capitalize font-semibold text-gray-700">
-                  total job
-                </h3>
-                <h2 className="block text-base sm:text-xl md:text-3xl lg:text-4xl font-bold text-gray-700">
-                  20
-                </h2>
-                <p className="text-base font-normal text-gray-500">
-                  +30 This week
-                </p>
-              </div>
+          <Card className="sm:p-5 shadow flex items-center col-span-full sm:col-span-1 bg-white w-full rounded-xl">
+            <div className="space-y-3 md:space-y-6 w-full">
+              <h3 className=" capitalize text-base sm:text-xl font-semibold text-gray-700">total job</h3>
+              <h2 className="block text-base sm:text-xl md:text-3xl lg:text-4xl font-bold text-gray-700">
+                20
+              </h2>
+              <p className="text-base font-normal text-gray-500">+30 This week</p>
+            </div>
 
-              <div className="self-end">
-                <GraphIcon className="h-24" />
-              </div>
-            </Card>
-          </div>
-          <Calendar />
+            <div className="self-end">
+              <GraphIcon className="h-32" />
+            </div>
+          </Card>
         </div>
 
         {/* Requested Jobs starts*/}
@@ -77,4 +62,4 @@ const TalentDashBoard: React.FC = () => {
   );
 };
 
-export default TalentDashBoard
+export default TalentDashBoard;
